77c8901663999fe99cd4b12f84c6f13f7f88f172
Neel Adwani <nprosofficial@gmail.com>
2021-11-14 13:13:22 +0000

    render template

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('app', template_folder="templates", static_folder="static")
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template('index.html')
 
 app.run(host='0.0.0.0', port=8080)
