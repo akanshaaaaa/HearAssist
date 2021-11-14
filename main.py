3621fa38bed28acaf854757eb123e00e43d3f95f
Neel Adwani <nprosofficial@gmail.com>
2021-11-14 13:27:47 +0000

    added GET endpoint

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from replit import db
 
 app = Flask('app', template_folder="templates", static_folder="static")
 
-@app.route('/')
+lst = []
+
+@app.route('/', methods = ['GET'])
 def hello_world():
+  text = request.args.get('text')
+  print(text)
   return render_template('index.html')
 
 app.run(host='0.0.0.0', port=8080)
